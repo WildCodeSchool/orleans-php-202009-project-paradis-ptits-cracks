3d6f2060e21526508e6ad0cb95698752be7a11d8
cedric-n <cedric.ngondo@gmail.com>
2020-10-29 12:33:00 +0100

    Add a carousel and create a class for notre elevage

---
+++ src/Controller/BreedingController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+class BreedingController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('/Breeding/breeding.html.twig');
+    }
+}
