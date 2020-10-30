9ce6ec990c51db7d4c87dcf01f9789d74a02b586
Sandra-Louchart <sandra.louchart@gmail.com>
2020-10-30 10:32:45 +0100

    travis

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: sylvain
@@ -15,8 +16,7 @@ namespace App\Model;
 class ItemManager extends AbstractManager
 {
     /**
-     *
-     */
+     **/
     const TABLE = 'item';
 
     /**
@@ -60,7 +60,7 @@ class ItemManager extends AbstractManager
      * @param array $item
      * @return bool
      */
-    public function update(array $item):bool
+    public function update(array $item): bool
     {
 
         // prepared request
