bdfd2504491021015f69f0127b4e95554713610a
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-11-18 14:44:04 +0100

    create admin homepage

--- src/Model/DogManager.php
+++ src/Model/DogManager.php
@@ -35,7 +35,6 @@ class DogManager extends AbstractManager
 
     /**
      * @return array
-     * @SuppressWarning(PHPMD.ExcessivePublicCount)
      */
     public function selectAllDogData(): array
     {
